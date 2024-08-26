5b67b18355c1623e5b5f62c1fef458c89801ed11
qasim ali <muhammadqasimali96@gmail.com>
2024-08-26 15:43:25 +0500

    feat: dashboard users displayed

--- src/components/global/Loader.jsx
+++ src/components/global/Loader.jsx
@@ -2,7 +2,7 @@ import { HashLoader } from "react-spinners";
 
 const Loader = () => {
   return (
-    <div className="w-screen h-screen fixed top-0 left-0 flex flex-row items-center justify-center">
+    <div className="flex flex-row items-center justify-center mt-20 w-full">
       <HashLoader color="#B6B5FF" size={50} loading={true} />
     </div>
   );
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,15 +1,42 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchDashboardStats } from "../../redux/dashboardSlice";
+import Loader from "../../components/global/Loader";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
+  const { totalUsers, totalTeams, loading, error } = useSelector(
+    (state) => state.dashboard
+  );
 
   useEffect(() => {
     dispatch(fetchDashboardStats());
   }, [dispatch]);
 
-  return <div>Dashboard</div>;
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p className="text-text">Something went wrong...</p>;
+  }
+
+  return (
+    <div className="text-text">
+      <h1 className="font-semibold text-xl text-black/60">Stats</h1>
+      <div className="flex flex-row gap-6 mt-5">
+        <div className="flex flex-col gap-4 p-5 w-52 border rounded-lg">
+          <p className="text-lg text-textSecondary">Total Users</p>
+          <p className="font-semibold text-4xl">{totalUsers}</p>
+        </div>
+
+        <div className="flex flex-col gap-4 p-5 w-52 border rounded-lg">
+          <p className="text-lg text-textSecondary">Total Teams</p>
+          <p className="font-semibold text-4xl">{totalTeams}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
